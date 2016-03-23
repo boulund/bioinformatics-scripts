54f020d71abfab746e327b27ece73b2c25daa648
Fredrik Boulund <fredrik.boulund@chalmers.se>
2016-03-23 13:53:53 +0100

    Added new print_sequence_lengths.py that uses new read_fastq function.

--- print_sequence_lengths.py
+++ print_sequence_lengths.py
@@ -6,63 +6,24 @@ from sys import argv, exit
 import os
 import fnmatch
 
+from read_fasta import read_fasta, read_fastq
+
 if len(argv) < 2:
-    print "usage: script.py REFSEQDIR"
+    print "usage: script.py FILE [...]"
     exit()
 
-
-def find_files(directory, pattern):
-    """Generator that yields files by recursively searching a dir with a glob pattern."""
-    for root, subfolders, files in os.walk(directory, followlinks=True):
-        for basename in files:
-            if fnmatch.fnmatch(basename, pattern):
-                filename = os.path.join(root, basename)
-                yield filename
-
-
-def read_fasta(filename, keep_formatting=True):
-    """Read sequence entries from FASTA file
-    NOTE: This is a generator, it yields after each completed sequence.
-    Usage example:
-    for header, seq in read_fasta(filename):
-        print ">"+header
-        print seq
-    """
-
-    with open(filename) as fasta:
-        line = fasta.readline().rstrip()
-        if not line.startswith(">"):
-            raise IOError("Not FASTA format? First line didn't start with '>'")
-        if keep_formatting:
-            sep = "\n"
-        else:
-            sep = ""
-        first = True
-        seqlen = 0
-        header = ""
-        while fasta:
-            if line == "": #EOF
-                yield seqlen, header
-                break
-            elif line.startswith(">") and not first:
-                yield seqlen, header
-                header = line.split()
-                seqlen = 0
-            elif line.startswith(">") and first:
-                header = line.split()
-                first = False
-            else:
-                seqlen += len(line.rstrip())
-            line = fasta.readline()
-
-
-
 if __name__ == "__main__":
-    seqinfo = []
-    for fna in find_files(argv[1], "*.fna"):
-        for info in read_fasta(fna, keep_formatting=True):
-            seqinfo.append(info)
-
-    seqinfo.sort(key=lambda x: x[0])
-    for length, header in seqinfo:
-        print "{}\t{}\t{}".format(length, header[0][1:], " ".join(header[1:]))
+    for filename in argv[1:]:
+        if filename.endswith((".fasta", ".fa", ".fna", ".pfa")):
+            file_is_fasta = True
+        elif filename.endswith((".fastq", ".fq")):
+            file_is_fasta = False
+        else:
+            print("WARNING: Unknown filetype of {}".format(filename))
+            continue
+        if file_is_fasta:
+            for header, seq in read_fasta(filename):
+                print("{}\t{}".format(len(seq), header))
+        else:
+            for header, seq, _, _ in read_fastq(filename):
+                print("{}\t{}".format(len(seq), header[1:]))
