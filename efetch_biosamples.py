e9b0e1d45f7a16b441809fe9281899a415e1c289
Fredrik Boulund <fredrik.boulund@chalmers.se>
2016-06-20 09:28:54 +0200

    [New script] Added efetch_biosamples.py

---
+++ efetch_biosamples.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3.5
+# Fredrik Boulund (c) 2016
+# Efetch BioSample data from GenBank 
+
+from sys import argv, exit
+from collections import OrderedDict
+from requests import get
+import argparse
+
+
+def parse_args():
+    """
+    Parse command line arguments.
+    """
+    desc = "Efetch BioSample information from NCBI. Fredrik Boulund (c) 2016."
+
+    parser = argparse.ArgumentParser(description=desc)
+
+    parser.add_argument("BIOSAMPLES", 
+            help="Text file with one BioSample accession per line.")
+    parser.add_argument("-a", "--attribute", nargs="+", dest="attributes",
+            help="""Attributes to include in output (can be specified 
+            multiple times). Enclose attributes in quotation marks!""")
+    parser.add_argument("-o", "--output", dest="output",
+            default="biosamples_data.txt",
+            help="Output filename (tab separated text).")
+
+    if len(argv) < 2:
+        print(help_text)
+        exit()
+
+    return parser.parse_args()
+
+
+def parse_biosamples(filename):
+    """
+    Parse biosample annos from file.
+    """
+
+    with open(filename) as f:
+        for line in f:
+            biosample = line.strip()
+            yield biosample
+
+
+def parse_attributes(text):
+    """
+    Return attribute:value pairs from NCBI E-fetch text response.
+    """
+    lines = text.split("\n")
+    desc = lines[0].split(":")[1].strip()
+    identifiers = lines[1][12:].split(";")
+    organism = lines[2].split(":")[1].strip()
+    attributes = {}
+    accession = ""
+    for line in lines[4:]:
+        if line.strip().startswith("/"):
+            attrib, value = line.strip("/ ").split("=")
+            attributes[attrib] = value.replace('"', '')
+        if line.startswith("Accession"):
+            accession = line.split(":")[1].strip()
+    attribs = [("Description", desc),
+               ("Identifiers", identifiers),
+               ("Organism", organism),
+               ("Accession", accession),
+               ("Attributes", attributes),
+               ]
+    return OrderedDict(attribs)
+
+
+def efetch_biosample(accno, retry_attempts=2):
+    """
+    Use NCBI E-utils efetch to get info for biosample.
+    Returns an empty string if no match is found for accno.
+    """
+    payload = {"db": "biosample", 
+               "id": accno,
+               "retmode": "text"}
+
+    result = get("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi", params=payload)
+    if result.ok:
+        if result.text.startswith("Error"):
+            print("WARNING: error occured for {}".format(accno))
+            return 
+        else:
+            return result.text
+    else:
+        print("WARNING: no match for {}".format(accno))
+        return 
+
+
+def write_output(biosamples, outfilename, attribute_keys=None):
+    """
+    Writes BioSample attributes to tab separated text file.
+    """
+
+    if attribute_keys is None:
+        keys = ["isolation source", 
+                "host", 
+                "host disease", 
+                "geographic location", 
+                "collection date"]
+    else:
+        keys = attribute_keys
+    header = "Accession\tOrganism\tIdentifiers\tDescription"
+    for key in keys:
+        header = header+"\t"+key
+    with open(outfilename, 'w') as outfile:
+        outfile.write(header+"\n")
+        for biosample in biosamples:
+            outfile.write(biosample["Accession"]+"\t")
+            outfile.write(biosample["Organism"]+"\t")
+            outfile.write(" ".join(biosample["Identifiers"])+"\t")
+            outfile.write(biosample["Description"]+"\t")
+            for key in keys:
+                try:
+                    outfile.write(biosample["Attributes"][key]+"\t")
+                except KeyError:
+                    outfile.write("-\t")
+            outfile.write("\n")
+
+
+def main(biosamples_file, outputfile, attribute_keys):
+    """
+    Main function.
+    """
+    biosamples = []
+    for biosample in parse_biosamples(biosamples_file):
+        biosample_info = efetch_biosample(biosample)
+        if biosample_info:
+            biosamples.append(parse_attributes(biosample_info))
+    write_output(biosamples, outputfile, attribute_keys=attribute_keys)
+
+
+if __name__ == "__main__":
+    options = parse_args()
+    main(options.BIOSAMPLES, options.output, options.attributes)
